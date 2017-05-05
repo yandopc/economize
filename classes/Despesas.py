4d0fc2b6145f9e1dfe2e2f7bcb102f73d0e8d1f9
Yan Vancelis <yanvancelis@gmail.com>
2017-05-05 13:53:21 -0300

    Despesa funciona

--- classes/Despesas.py
+++ classes/Despesas.py
@@ -7,11 +7,14 @@ class Despesas (Lancamento):
     def __init__ (self, categoria: Categoria, valor:float, descricao:str):
         #super (Despesas, self).__init__(valor, descricao)
         Lancamento.__init__(self, valor, descricao)
-        self.__categoria = Categoria
+        self.__categoria = categoria
 
     @property
     def categoria (self):
-        return self.__categoria
+        return (
+            self.__categoria.nome,
+            self.__categoria.descricao
+            )
 
     @categoria.setter
     def categoria (self, categoria:Categoria):
