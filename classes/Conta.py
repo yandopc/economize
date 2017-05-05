3682f541239903b0c41928fd614a7e12eb62c156
Marcos Alencar <marcoscarvalho1997@gmail.com>
2017-05-05 11:24:04 -0300

    Modificando contas

--- classes/Conta.py
+++ classes/Conta.py
@@ -1,22 +1,57 @@
 #!/usr/bin/env python
 
 class Conta:
-    def __init__(self,numero:int, valorMinimo:float = 0.0, valorDesejavel:float = 0, saldo:float=0.0):
-        self.__numero = numero
-        self.__valorMinimo = valorMinimo
-        self.__valorDesejavel = valorDesejavel
-        self.__saldo = saldo
+    num_contas = 0
+    def __init__(self, valor_minimo:float = 0.0, valor_desejavel:float = 0, saldo:float=0.0):
+        Conta.num_contas+=1
+        self.__numero = Conta.num_contas
+        self.valor_minimo = valor_minimo
+        self.valor_desejavel = valor_desejavel
+        self.saldo = saldo
 
     @property
     def numero (self):
         return self.__numero
 
-    @numero.setter
-    def numero (self,numero:int):
-        if isinstance (numero,int):
-            self.__numero = numero
+    @property
+    def valor_minimo(self):
+        return self.__valor_minimo
+    @valor_minimo.setter
+    def valor_minimo (self, valorMinimo:float):
+        if (isinstance (valor_minimo,int) or isinstance (valor_minimo,int)):
+            if(valor_minimo > 0.0):
+                self.__valor_minimo = valor_minimo
+            else:
+                raise ValueError
         else:
             raise AttributeError
 
-    def valorMinimo (self, valorMinimo:float):
-        self.__valorMinimo = valorMinimo
+    @property
+    def valor_desejavel(self):
+        return self.__valor_desejavel
+
+    @valor_desejavel.setter
+    def valor_desejavel(self, valor_desejavel:float):
+    if (isinstance (valor_desejavel,int) or isinstance (valor_desejavel,int)):
+        if(valor_desejavel > 0.0):
+            self.__valor_desejavel = valor_desejavel
+        else:
+            raise ValueError
+    else:
+        raise AttributeError
+
+    @property
+    def saldo(self):
+        return self.__saldo
+
+    @saldo.setter
+    def saldo (self, saldo:float):
+    if (isinstance (saldo,int) or isinstance (saldo,int)):
+        if(saldo > 0.0):
+            self.__saldo = saldo
+        else:
+            raise ValueError
+    else:
+        raise AttributeError
+
+    
