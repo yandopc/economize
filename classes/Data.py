5994c81ab6a9ecba8bfe56da0e60686c353e4e01
Marcos Alencar <marcoscarvalho1997@gmail.com>
2017-05-05 12:24:27 -0300

    Classe data

---
+++ classes/Data.py
@@ -0,0 +1,20 @@
+from datetime import *
+#str(hj.day)+"/"+str(hj.month)+"/"+str(hj.year)
+class Data():
+    def __init__(self, dia:int=date.today().day, mes:int, ano:int):
+        self.dia = dia
+        self.mes = mes
+        self.ano = ano
+
+    @property
+    def dia(self):
+        return self.__dia
+    @dia.setter
+    def dia(self,dia:int):
+        if(isinstance (dia:int)):
+            if(dia >0 && dia <=31):
+                self.__dia = dia
+            else:
+                raise ValueError
+        else:
+            raise AttributeError
