8f5ee640011b2f13a436525cbdddd1105f2e1ae1
Alex Frederico <alexfrederico94@yahoo.com.br>
2017-05-05 12:38:26 -0300

    add categoria

---
+++ classes/Categoria.py
@@ -0,0 +1,23 @@
+class Categoria(object):
+	def __init__(self, nome:str, descricao:str=""):
+		self.nome=nome
+		self.descricao=descricao
+	@property
+	def nome(self):
+		return self.__nome
+	@nome.setter
+	def nome(self, nome):
+		if isinstance(nome, str):
+			self.__nome=nome
+		else:
+			raise AttributeError
+	@property
+	def descricao(self):
+		return self.__descricao
+	@descricao.setter
+	def descricao(self, descricao):
+		if isinstance(descricao, str):
+			self.__descricao=descricao
+		else:
+			raise AttributeError
+
