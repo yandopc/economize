8103eb2c3afca5792bdb735655ea430f951f7b80
Yan Vancelis <yanvancelis@gmail.com>
2017-05-05 20:30:00 -0300

    Classe Fixas adicionada

--- classes/Fixas.py
+++ classes/Fixas.py
@@ -1,1 +1,21 @@
 #!/usr/bin/env python
+
+from Despesas import *
+from Data import *
+
+class Fixas (Despesas):
+    def __init__ (vencimento:Data, categoria: Categoria, valor:float, descricao:str):
+        Despesas.__init__(self, categoria, valor, descricao)
+        self.__vencimento = vencimento
+
+    @property
+    def vencimento (self):
+        return vencimento
+
+    @vencimento.setter
+    def vencimento (vencimento: Data):
+        if isinstance (vencimento, Data):
+            self.__vencimento = vencimento
+        else:
+            raise AttributeError
+    
