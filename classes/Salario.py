abbaae1963dcb0290cee6ad80abfe58ebcb2143c
Alex Frederico <alexfrederico94@yahoo.com.br>
2017-05-06 18:59:00 -0300

    atualizado e salario precisa ser ajeitado

--- classes/Salario.py
+++ classes/Salario.py
@@ -1,21 +1,45 @@
 #!/usr/bin/env python
 
+from Data import *
+
 class Salario:
-    def __init__ (self, data:str, valor:float = 0, descr:str = 0):
-        self.__data = data
-        self.__valor = valor
-        self.__descricao = descr
+    def __init__ (self, data:Data, valor:float, descricao:str = ""):
+        self.data = data
+        self.valor = valor
+        self.descricao = descricao
 
     @property
-    def salario (self):
+    def data (self):
+        return self.__data
+
+    @data.setter
+    def data (self, data:Data):
+		if isinstance (data, Data):
+			self.__data = data
+        else:
+            raise AttributeError
+
+    @property
+    def descricao (self):
+        return self.__descricao
+
+    @descricao.setter
+    def descricao (self, descricao:str):
+		if isinstance (descricao, str):
+			self.__descricao = descricao
+        else:
+            raise AttributeError
+
+    @property
+    def valor (self):
         return self.__valor
 
-    @salario.setter
-    def salario (self, salario:float):
-        if salario > 0:
-            if isinstance (salario, float):
-                self.__salario = salario
+    @valor.setter
+    def valor (self, valor:float):
+		if isinstance (valor, float) or isinstance(valor, int):
+			if valor >= 0:
+                self.__valor = valor
             else:
-                raise AttributeError
+                raise ValueError
         else:
-            raise ValueError
+            raise AttributeError
